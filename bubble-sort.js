f12c8b9da4c9b543e53a78d327bc4d211a89a374
McBride, Dustin <Dustin.McBride@agilysys.com>
2017-08-26 12:40:29 -0700

    better bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -3,25 +3,19 @@
 let sortMe = [9, 8, 7, 3, 5, -1, 200, -78, 4, 3];
 
 function bubbleSort(arr) {
-  let sorted = false;
 
-  while (!sorted) {
-    let changes = false;
-    for(let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i +1]) {
-        changes = true;
-        let newarr = arr.slice();
-        newarr[i] = arr[i + 1];
-        newarr[i + 1] = arr[i];
-        arr = newarr;
-      };
-      sorted = !changes;
+  for(let i = 0; i <= arr.length - 2; i++) {
+    for(let j = 0; j <= arr.length - 2 - i; j++) {
+      if(arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
-  return arr;
+  return arr
 }
 
 console.log(bubbleSort(sortMe));
-
 
 
